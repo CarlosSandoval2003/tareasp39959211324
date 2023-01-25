125dd20092e22af4e288f791ee653ca00fcc8bf6
CarlosSandoval2003 <jawicho@outlook.com>
2023-01-25 07:42:18 -0600

    Programa Operaciones Basicas Modificado

--- OperacionesBasicas/OperacionesBasicas/src/main/java/Operaciones/OperacionesBasicas.java
+++ OperacionesBasicas/OperacionesBasicas/src/main/java/Operaciones/OperacionesBasicas.java
@@ -18,15 +18,15 @@ public class OperacionesBasicas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        float opc=0,dato1=0,dato2=0,result=0;
-
-        System.out.println("1.-sumar    2.-restar   3.-multiplicar  4.-dividir  otro numero.-salir");
-        System.out.println("seleccione una opcion: ");
-        opc=Integer.parseInt(JOptionPane.showInputDialog("ingrese opcion: "));
+        float opc=0,dato1=0,dato2=0,result=0, i = 0;
+        
+        while (i<1)
+        {
+        opc=Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opcion: " + "1.-Sumar    2.-Restar   3.-Multiplicar  4.-Dividir"));
 
         if(opc>0&&opc<5){
-            dato1=Integer.parseInt(JOptionPane.showInputDialog("ingrese primer numero: "));
-            dato2=Integer.parseInt(JOptionPane.showInputDialog("ingrese segundo numero numero: "));
+            dato1=Integer.parseInt(JOptionPane.showInputDialog("Ingrese primer numero: "));
+            dato2=Integer.parseInt(JOptionPane.showInputDialog("Ingrese segundo numero numero: "));
         if(opc!=0){
             if(opc==1)
                 result=dato1+dato2;
@@ -36,13 +36,17 @@ public class OperacionesBasicas {
                 result=dato1*dato2;
             if(opc==4)
                 result=dato1/dato2;
-            System.out.print("el resultado es: ");
-            System.out.println(result);
-        }
+        } 
         JOptionPane.showMessageDialog(null, "el resultado es: "+result);
         }
-            System.out.println("saliendo...");
-            JOptionPane.showMessageDialog(null,"adios");        
+          opc=Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opcion: " + "1.-Realizar otra operación    2.-Salir" ));
+            if (opc==1){
+                i = 0;
+            } else {
+                i = 2;
+            }  
+                 
     }
-    
+    JOptionPane.showMessageDialog(null,"Adios");   
+    }
 }
